26e7374f2ab5c315064fb91c8e3043f978ad147e
Esdras Oliveira <92229240+EsdrasOliver@users.noreply.github.com>
2024-02-07 11:36:44 -0300

    aula 1 do nlw react native

---
+++ my-app/src/app/_layout.tsx
@@ -0,0 +1,29 @@
+import { Slot } from "expo-router";
+import { SafeAreaView } from "react-native";
+import {
+    useFonts,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+} from "@expo-google-fonts/inter"
+import { Loading }  from "@/components/loading"
+
+export default function Layout() {
+    const [fontsLoader] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        Inter_600SemiBold,
+        Inter_700Bold
+    })
+
+    if(!fontsLoader) {
+        return <Loading />
+    }
+    
+    return (
+        <SafeAreaView className="flex-1 bg-slate-900">
+            <Slot />
+        </SafeAreaView>
+    )
+}
